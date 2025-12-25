43b3e2270195787601cace3654e5d9aaa4d59972
Mathieu Imfeld <imfeldma@gmail.com>
2025-12-25 17:53:01 +0100

    Refactoring
    Signed-off-by: Mathieu Imfeld <imfeldma@gmail.com>

--- src/kube_eng/config/cluster_config.py
+++ src/kube_eng/config/cluster_config.py
@@ -1,36 +1,57 @@
+import enum
+import pathlib
 import socket
 from typing import Any
 
-from pydantic import Field
+from pydantic import Field, computed_field
 
-from kube_eng import __helm_chart_path__
 from .root_config_aware import RootConfigAware
 
 
-class ClusterMeshConfig(RootConfigAware):
-    kind: str = Field(default="istio")
-    ns: str = Field(default="istio-system")
+class ClusterMeshKind(str, enum.Enum):
+    istio = "istio"
 
+class ClusterMeshConfig(RootConfigAware):
+    enabled: bool = Field(default=True)
+    kind: ClusterMeshKind = Field(default=ClusterMeshKind.istio)
+    ns: str = Field(default="istio-system")
+    istio_gateway_api_crd: str = Field(
+        default="https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml"
+    )
 
 class ClusterPKIConfig(RootConfigAware):
     ns: str = Field(default="cert-manager")
     crd: str = Field(
         default="https://github.com/cert-manager/cert-manager/releases/download/v1.17.1/cert-manager.crds.yaml"
     )
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-cert-manager"))
+    key_type: str = Field(default="ECC")
+    key_curve: str = Field(default="secp384r1")
+    key_size: int = Field(default=4096)
+    crt_validity: str = Field(default="+825d")
+
+    @computed_field
+    @property
+    def config_path(self) -> pathlib.Path:
+        """
+        Directory to store PKI files in.
+        Returns:
+            Path to the PKI directory.
+        """
+        return self._root_config.config_path / "pki"
+
+class ClusterEdgeKindEnum(str, enum.Enum):
+    istio = "istio"
+    istio_gateway_api = "istio-gateway-api"
+    ingress = "ingress"
 
 
 class ClusterEdgeConfig(RootConfigAware):
-    kind: str = Field(default="istio-gateway-api")
+    kind: ClusterEdgeKindEnum = Field(default=ClusterEdgeKindEnum.ingress)
     name: str = Field(default="edge-ingress")
     ns: str = Field(default="edge")
-    external_domain: str = Field(default="k8s")
     ingress_repository: str = Field(default="docker.io/traefik")
-    ingress_tag: str = Field(default="v3.4.4")
-    istio_gateway_api_crd: str = Field(
-        default="https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml"
-    )
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-edge"))
+    ingress_tag: str = Field(default="v3.6")
+    ingress_hostname: str = Field(default="edge")
 
 
 class ClusterConfig(RootConfigAware):
@@ -39,12 +60,12 @@ class ClusterConfig(RootConfigAware):
     control_plane_nodes: int = Field(default=1)
     worker_nodes: int = Field(default=3)
 
-    pki: ClusterPKIConfig = Field(default_factory=ClusterPKIConfig)
     mesh: ClusterMeshConfig = Field(default_factory=ClusterMeshConfig)
+    pki: ClusterPKIConfig = Field(default_factory=ClusterPKIConfig)
     edge: ClusterEdgeConfig = Field(default_factory=ClusterEdgeConfig)
 
     def model_post_init(self, context: Any, /) -> None:
         super().model_post_init(context)
         # We want to have an unqualified hostname
         if self.name == socket.gethostname() and self.name.endswith(".local"):
-            self.name.replace(".local", "")
+            self.name = self.name.replace(".local", "")
--- src/kube_eng/config/stack_config.py
+++ src/kube_eng/config/stack_config.py
@@ -1,6 +1,5 @@
 from pydantic import Field
 
-from kube_eng import __helm_chart_path__
 from .root_config_aware import RootConfigAware
 
 
@@ -8,30 +7,22 @@ class StackPrometheusConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="prometheus")
     hostname: str = Field(default="prometheus")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-prometheus"))
-
 
 class StackAlloyConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="alloy")
     hostname: str = Field(default="alloy")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-alloy"))
-
 
 class StackLokiConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="loki")
     hostname: str = Field(default="loki")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-loki"))
-
 
 class StackKeycloakConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="keycloak")
     hostname: str = Field(default="keycloak")
     operator_version: str = Field(default="26.4.7")
-    operator_chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-keycloak-operator"))
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-operator"))
 
 
 class StackGrafanaConfig(RootConfigAware):
@@ -40,30 +31,18 @@ class StackGrafanaConfig(RootConfigAware):
     hostname: str = Field(default="grafana")
     client_id: str = Field(default="grafana")
     admin_user: str = Field(default="admin")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-grafana"))
 
 
 class StackJaegerConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="jaeger")
     hostname: str = Field(default="jaeger")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-jaeger"))
-
-
-class StackJaegerV2Config(RootConfigAware):
-    enabled: bool = Field(default=False)
-    ns: str = Field(default="jaeger")
-    hostname: str = Field(default="jaeger")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-jaeger-v2"))
-
 
 class StackKialiConfig(RootConfigAware):
     enabled: bool = Field(default=True)
     ns: str = Field(default="kiali")
     hostname: str = Field(default="kiali")
     version: str = Field(default="v2.18.0")
-    chart_ref: str = Field(default=str(__helm_chart_path__ / "kube-eng-kiali"))
-
 
 class StackConfig(RootConfigAware):
     prometheus: StackPrometheusConfig = Field(default_factory=StackPrometheusConfig)
@@ -72,5 +51,4 @@ class StackConfig(RootConfigAware):
     keycloak: StackKeycloakConfig = Field(default_factory=StackKeycloakConfig)
     grafana: StackGrafanaConfig = Field(default_factory=StackGrafanaConfig)
     jaeger: StackJaegerConfig = Field(default_factory=StackJaegerConfig)
-    jaeger_v2: StackJaegerV2Config = Field(default_factory=StackJaegerV2Config)
     kiali: StackKialiConfig = Field(default_factory=StackKialiConfig)
