ad8dfdb8e0e64e1ba8ba8d24105ca1ea563c7e7c
Mathieu Imfeld <imfeldma@gmail.com>
2026-03-27 10:33:05 +0100

    Transfer

--- src/kube_eng/config/cluster_config.py
+++ src/kube_eng/config/cluster_config.py
@@ -13,7 +13,7 @@ class ClusterCNIKindEnum(str, enum.Enum):
     cilium = "cilium"
 
 class ClusterCNIConfig(RootConfigAware):
-    kind: ClusterCNIKindEnum = Field(default=ClusterCNIKindEnum.kind)
+    kind: ClusterCNIKindEnum = Field(default=ClusterCNIKindEnum.cilium)
     exclusive: bool = Field(default=False, description="If true, only one CNI plugin can be active at a time")
     ui: bool = Field(default=False, description="If true, deploy CNI UI")
     hostname: str = Field(default="cni", description="CNI UI hostname, if applicable")
@@ -29,25 +29,12 @@ class ClusterMeshConfig(RootConfigAware):
     ns: str = Field(default="istio-system")
 
 class ClusterPKIConfig(RootConfigAware):
+    enabled: bool = Field(default=True)
     ns: str = Field(default="cert-manager")
     crd: str = Field(
         default="https://github.com/cert-manager/cert-manager/releases/download/v1.17.1/cert-manager.crds.yaml"
     )
-    key_type: str = Field(default="ECC")
-    key_curve: str = Field(default="secp384r1")
-    key_size: int = Field(default=4096)
-    crt_validity: str = Field(default="+825d")
-
-    @computed_field
-    @property
-    def config_path(self) -> pathlib.Path:
-        """
-        Directory to store PKI files in.
-        Returns:
-            Path to the PKI directory.
-        """
-        return self._root_config.config_path / "pki"
-
+    hostname: str = Field(default="pki")
 
 class ClusterEdgeKindEnum(str, enum.Enum):
     istio = "istio"
@@ -61,23 +48,46 @@ class ClusterEdgeConfig(RootConfigAware):
     gateway_api_crds: str = Field(
         default="https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/experimental-install.yaml"
     )
-    traefik_repository: str = Field(default="docker.io/traefik")
-    traefik_tag: str = Field(default="v3.6")
-    traefik_hostname: str = Field(default="edge")
-    traefik_dashboard_hostname: str = Field(default="dashboard")
+
+    @computed_field
+    @property
+    def gateway_class(self) -> str:
+        match self.kind:
+            case ClusterEdgeKindEnum.istio:
+                return 'istio'
+            case ClusterEdgeKindEnum.istio_gateway_api:
+                return 'istio'
+            case ClusterEdgeKindEnum.traefik:
+                return 'traefik'
+            case _:
+                raise ValueError(f"Unknown edge kind: {self.kind}")
+
+class ClusterOIDCConfig(RootConfigAware):
+    client_id: str = Field(default="kubernetes")
+    username_claim: str = Field(default="preferred_username")
+    groups_claim: str = Field(default="groups")
+
+    @computed_field
+    @property
+    def issuer_url(self) -> str:
+        """
+        Computed IDP issuer URL for the cluster
+        Returns:
+            Computed IDP issuer URL for the cluster
+        """
+        return f"https://{self._root_config.host.idp.name}.{self._root_config.host.dns.zone}:{self._root_config.host.idp.host_port }/realms/master"
 
 class ClusterConfig(RootConfigAware):
     name: str = Field(description="Name of the cluster", default_factory=socket.gethostname)
-
     pod_subnet_cidr: str = Field(default="10.244.0.0/16")
     service_subnet_cidr: str = Field(default="10.96.0.0/12")
-
     control_plane_nodes: int = Field(default=1)
     worker_nodes: int = Field(default=3)
 
     cni: ClusterCNIConfig = Field(default_factory=ClusterCNIConfig)
     mesh: ClusterMeshConfig = Field(default_factory=ClusterMeshConfig)
     pki: ClusterPKIConfig = Field(default_factory=ClusterPKIConfig)
+    oidc: ClusterOIDCConfig = Field(default_factory=ClusterOIDCConfig)
     edge: ClusterEdgeConfig = Field(default_factory=ClusterEdgeConfig)
 
     def model_post_init(self, context: Any, /) -> None:
